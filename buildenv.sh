2c03efd239a6e065bafa3b9195c99f49a4d5aff5
Takafumi Nose <ahya365@gmail.com>
2012-02-06 06:05:09 +0900

    initial commit

---
+++ buildenv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "alias sl=ls" >> .bashrc
+echo "alias ks=ls" >> .bashrc
+echo "alias lks=ls" >> .bashrc
+echo "export PATH=~/usr/bin:$PATH" >> .bashrc
+echo "export LD_LIBRARY_PATH=~/usr/lib:$LD_LIBRARY_PATH" >> .bashrc
+
